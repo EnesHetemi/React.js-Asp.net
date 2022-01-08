4bc6796753e5188e4d965c71e211350aefe4bdce
Enes Hetemi <you@example.com>
2022-01-08 18:22:01 +0100

    Krijimi i menu dhe disa gjera tjera

---
+++ react-movies/src/route-config.ts
@@ -0,0 +1,43 @@
+import CreateGenre from "./Genres/CreateGenre";
+import EditGenre from "./Genres/EditGenre";
+import IndexGenres from "./Genres/indexGenres";
+
+import IndexActors from "./actors/IndexActors";
+import CreateActor from "./actors/CreateActor";
+import EditActor from "./actors/EditActor";
+
+import IndexMovieTheaters from "./movietheaters/IndexMovieTheaters";
+import CreateMovieTheater from "./movietheaters/CreateMovieTheater";
+import EditMovieTheater from "./movietheaters/EditMovieTheater";
+
+import CreateMovie from "./Movies/CreateMovie";
+import EditMovie from "./Movies/EditMovie";
+import FilterMovies from "./Movies/FilterMovies";
+
+import LandingPage from "./Movies/LandingPage";
+import RedirectToLandingPage from './utils/RedirectToLandingPage';
+
+
+const routes = [
+    {path: '/genres', component: IndexGenres, exact: true},
+    {path: '/genres/create', component: CreateGenre},
+    {path: '/genres/edit/:id(\\d+)', component: EditGenre},
+
+    {path: '/actors', component: IndexActors, exact: true},
+    {path: '/actors/create', component: CreateActor},
+    {path: '/actors/edit/:id(\\d+)', component: EditActor},
+
+    {path: '/movietheaters', component: IndexMovieTheaters, exact: true},
+    {path: '/movietheaters/create', component: CreateMovieTheater},
+    {path: '/movietheaters/edit/:id(\\d+)', component: EditMovieTheater},
+
+    {path: '/movies/create', component: CreateMovie},
+    {path: '/movies/edit/:id(\\d+)', component: EditMovie},
+    {path: '/movies/filter', component: FilterMovies},
+
+    {path: "/", component: LandingPage, exact: true},
+    {path: '*', component: RedirectToLandingPage}
+
+];
+
+export default routes;
