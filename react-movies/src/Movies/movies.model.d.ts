07f3b5473a90ca6e2eae961e453b6c5d0a840d1e
Enes Hetemi <you@example.com>
2022-01-09 15:32:07 +0100

    Commit

--- react-movies/src/Movies/movies.model.d.ts
+++ react-movies/src/Movies/movies.model.d.ts
@@ -1,7 +1,19 @@
-export interface movieDTO{
+export interface movieDTO {
     id: number;
     title: string;
     poster: string;
+}
+
+export interface movieCreationDTO {
+    title: string;
+    inTheaters: boolean;
+    trailer: string;
+    releaseDate?: Date;
+    poster?: File;
+    posterURL?: string;
+    genresIds?: number[];
+    movieTheatersIds?: number[];
+    actors?: actorMovieDTO[];
 }
 
 export interface landingPageDTO {
